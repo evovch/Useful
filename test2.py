4bbfa108af10f108e556c300b90f2ea4c557b240
evovch <evovch@gmail.com>
2017-04-06 10:21:29 +0300

    Add pyparsing example.

---
+++ test2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from pyparsing import Word, OneOrMore
+
+inFilename = 'out1.txt'
+FIN = open(inFilename, 'r')
+TEXT = FIN.read()
+
+myDigits = '0123456789'
+eng_alphas = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+rus_alphas = 'йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ'
+punctuation = '.,:;'
+myPrintables = myDigits + eng_alphas + rus_alphas + punctuation
+
+aWord = Word(myPrintables)
+someText = OneOrMore(aWord)
+
+outputText = someText.parseString(TEXT)
+
+print outputText
