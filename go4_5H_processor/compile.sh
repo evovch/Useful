76b097267dd96552a38948707c343ed22ecd4a21
evovch <evovch@gmail.com>
2018-04-15 13:28:25 +0300

    Restructure.

--- go4_5H_processor/compile.sh
+++ go4_5H_processor/compile.sh
@@ -11,8 +11,11 @@ cmake ../
 make -j
 cd -
 
-cp -v build/*.so .
-cp -v build/*.pcm .
-cp -v build/*.rootmap .
+#cp -v build/src/*.so .
+#cp -v build/src/*.pcm .
+#cp -v build/src/*.rootmap .
+ln -s build/src/libUserAnalysis.so
+ln -s build/src/libUserAnalysis_rdict.pcm
+ln -s build/src/libUserAnalysis.rootmap
 
 echo -e "\e[1m\e[32mCompilation finished.\e[0m"
--- go4_5H_processor/doclean.sh
+++ go4_5H_processor/doclean.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-rm -fv libUserAnalysis.so libUserAnalysis_rdict.pcm libUserAnalysis.rootmap
+#rm -fv libUserAnalysis.so libUserAnalysis_rdict.pcm libUserAnalysis.rootmap
+unlink libUserAnalysis.so
+unlink libUserAnalysis_rdict.pcm
+unlink libUserAnalysis.rootmap
+
 rm -fv out.txt err.txt
 rm -fv summary.txt
 rm -fv outputUnpacking.root outputMonitoring.root outputStep2.root
